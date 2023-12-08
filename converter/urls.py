2c95e4c8c78345fad23e8b3d9b18af0f72512d76
Jonathan Nicolas <weptpear394@gmail.com>
2023-12-08 04:13:06 -0300

    done

---
+++ converter/urls.py
@@ -0,0 +1,10 @@
+
+import length
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('length/', include('length.urls'))
+]
---
+++ length/tests/test_length.py
@@ -0,0 +1,31 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+
+class TestLengthConversion(TestCase):
+
+    def setUp(self):
+       
+        self.client = Client()
+        self.url = reverse("length:convert")
+        
+    
+    def test_centimetre_to_metre_conversion(self):
+        """
+        Tests conversion of centimetre measurements to metre.
+        """
+        data = {
+            "input_unit": "centimetre",
+            "output_unit": "metre",
+            "input_value": round(8096.894, 3)
+        }
+        response = self.client.get(self.url, data)
+        self.assertContains(response, 80.969)
+        
+    def test_centimetre_to_mile_conversion(self):
+       data = {
+           "input_unit": "centimetre",
+           "output_unit": "mile",
+           "input_value": round(985805791.3527409, 3)
+       }
+       response = self.client.get(self.url, data)
+       self.assertContains(response, 6125.511)
